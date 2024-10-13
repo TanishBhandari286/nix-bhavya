b0a0a09d40bd32782feb9bb2aa78c5d009f45239
Tanish <tanishbhandari@Tanishs-MacBook-Air.local>
2024-10-13 16:47:16 +0530

    copy krna seekh

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
+}
